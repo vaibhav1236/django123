1d43cd537a0e56a1c6f288a6c039a90802d3bf0f
vaibhav1236 <87272843+vaibhav1236@users.noreply.github.com>
2021-08-26 06:26:54 +0530

    chsngr

--- Krishna/settings.py
+++ Krishna/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#tc+1-yg_jrfjp7%=j=tf^dxp#!zugpm)q@5zkqqc$x=fx8%s5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','hare-krishna.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','git123.herokuapp.com']
 
 
 # Application definition
